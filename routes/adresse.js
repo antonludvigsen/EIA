10ed9be55cf627ef77680314f1d71cffe0755dc1
Anton Ludvigsen <antonludvigsen03@gmail.com>
2026-04-28 11:11:41 +0200

    tilføjelse af adresse.js

--- routes/adresse.js
+++ routes/adresse.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router(); /* oprettes til at have sine egne routes - mønster for at holde routes opdelt på funktionalitet i stedet for at have alle routes i server.js. */
 const adresseController = require('../controllers/adresseController'); 
 
-router.get('/søg', adresseController.findAdresse); /* definerer at en GET-request til /søg skal kalde findAdresse-metoden i controlleren. Bemærk at vi ikke skriver findAdresse() med parenteser, vi giver Express en reference til funktionen som den selv kalder med (req, res). */
+router.get('/soeg', adresseController.findAdresse); /* definerer at en GET-request til /soeg skal kalde findAdresse-metoden i controlleren. 
+
+Bemærk at vi ikke skriver findAdresse() med parenteser, vi giver Express en reference til funktionen som den selv kalder med (req, res). 
+
+Derudover angiver vi det som "soeg" og ikke "søg", da vi har at gøre med browseren her og det dermed er upraktisk at bruge danske tegn i URL-stien*/
 
 module.exports = router; /* eksportere routeren så server.js kan registrere den */
 
