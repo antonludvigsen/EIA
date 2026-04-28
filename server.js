b812dd01f5a1e953590d5825e6e1bc2607f0fd1f
Anton Ludvigsen <antonludvigsen03@gmail.com>
2026-04-28 09:25:32 +0200

    Ændringer i kommentare

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express'); /* importerer Express-frameworket. */
 const path = require('path'); /* importerer Node.js' indbyggede path-modul */
 
 const app = express(); /* opretter selve Express-applikationen. app er objektet vi hænger alle routes og middleware på. */
-const PORT = 3000; /* vi sætter porten til at være 3000 */
+const PORT = 3000; /* vi sætter porten til at være 3000, så når vi skal hoste kommer det på http://localhost:3000 */
 
 app.use(express.json()); /*  */
-app.use(express.static(path.join(__dirname, 'public'))); /* vi sætter den statiske visning til de ting der ligger i frontend-mappen */
+app.use(express.static(path.join(__dirname, 'public'))); /* vi sætter den statiske visning til de ting der ligger i public-mappen */
 
 app.listen(PORT, () => {
     console.log(`EIA-server kører på http://localhost:${PORT}`);
