266c5ce67abe17d3fc4ec020fc0cad77ae405649
Anton Ludvigsen <antonludvigsen03@gmail.com>
2026-04-29 10:04:19 +0200

    kommentering af test fra server.js Db forbindelse

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ app.listen(PORT, () => {
     console.log(`EIA-server kører på http://localhost:${PORT}`);
 });
 
-/* TEST af forbindelse til databasen når serveren startes */
-
+/* TEST af forbindelse til databasen når serveren startes
 const { poolForbindelse } = require('./database/connection');
 
 poolForbindelse
     .then(() => console.log('Furbundet til Azure SQL'))
     .catch(fejl => console.error('Databasefejl:', fejl));
+*/
