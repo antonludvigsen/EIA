c637e7f5b2d0a9e4317e097e85299013ba773ca0
Anton Ludvigsen <antonludvigsen03@gmail.com>
2026-04-29 13:04:42 +0200

    adressevalidering med inputfelt og søgeknap

--- public/js/adresse.js
+++ public/js/adresse.js
@@ -4,8 +4,14 @@ Der kaldes /api/adresse/soeg via fetch() og viser resultater */
 const soegefelt = document.getElementById('adresse-soegefelt');
 const resultatListe = document.getElementById('soege-resultater');
 
+/* vi sætter en variabel der kan bruges til at gemme den valgte adresse (så SØG-knappen kan bruge den) */
+let valgtAdresse = null;
+
 soegefelt.addEventListener('input', async function () { /* lytter på hvert enkelt tastetryk i søgefeltet. Hver gang brugeren taster et tegn, køres funktionen automatisk. Det er det der giver autocomplete-effekten. */
     const søgning = soegefelt.value;
+
+    /* hver gang brugeren trykker på søgefeltet, nulstilles valgt adresse */
+    valgtAdresse = null;
 
     /* Ryd listen hvis søgefeltet er tomt */
     if (!søgning || søgning.trim() === '') {
@@ -24,9 +30,41 @@ soegefelt.addEventListener('input', async function () { /* lytter på hvert enke
       for (let i = 0; i < data.length; i++) {
         const li = document.createElement('li');
         li.textContent = data[i].forslagstekst;
+
+        /* når brugeren klikker på et resultat */
+        li.addEventListener('click', function () {
+
+          /* vises den valgte adresse først statisk i søgefeltet */
+          soegefelt.value = data[i].forslagstekst;
+
+          /* gem adresseobjektet til at bruge videre */
+          valgtAdresse = data[i];
+
+          /* skjul resultatlisten */
+          resultatListe.innerHTML = '';
+        });
+
         resultatListe.appendChild(li);
       }
     } catch (fejl) {
         console.error('fejl ved adressesøgning', fejl);
     }
 });
+
+/* SØG-knappen */
+
+const søgknap = document.querySelector('.hero-soeg-knap'); /* henter søgknappen */
+
+søgknap.addEventListener('click', function () {
+
+  /* validering om den valgte adresse er fra listen */
+  if (!valgtAdresse) {
+    alert('Vælg venligst en adresse fra listen'); 
+    return;
+  }
+
+  /* herfra sender vi brugeren videre med den valgte adresse som URL parameter */
+  const adresseId = valgtAdresse.data.id;
+  window.location.href = `/ejendomsprofil.html?adresseId=${encodeURIComponent(adresseId)}`;
+});
+
